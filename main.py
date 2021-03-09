f261ec4b64790bfb9c1f9113b8c89f2b5a914dc9
Tower5954 <g.thompson5@live.co.uk>
2021-03-09 12:26:51 +0000

    n

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ root = tk.Tk()
 
 user_name = tk.StringVar()
 
-
 name_label = ttk.Label(root, text="Name: ")
 name_label.pack(side='left', padx=(0, 10))
 name_entry = ttk.Entry(root, width=15, textvariable=user_name)
 name_entry.pack(side='left')
 name_entry.focus()
-
 
 greet_button = ttk.Button(root, text='Greet', command=greet)
 greet_button.pack(side='left', fill='x', expand=True)
